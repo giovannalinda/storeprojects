9288617d84675186e3c20b132ad11f8bebfafd12
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-06-30 18:23:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const server = express();
 
+// Read
 server.get('/projects', (req, res) => { 
   return res.json({ message: 'Hello World'});
 })
+
+// Create
+server.post('/projects', (req, res) => {
+
+});
+
 
 server.listen(3000);
